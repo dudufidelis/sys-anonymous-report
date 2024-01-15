fda04157ff2eef815e201aed64a1ebe21dff083a
dudufidelis <duduecal12@gmail.com>
2024-01-15 12:56:10 -0300

    arquivo de configuração banco de dados

---
+++ database_setup.sql
@@ -0,0 +1,13 @@
+-- Script SQL para criar tabelas
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome_usuario VARCHAR(255) NOT NULL,
+    senha_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE denuncias (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    mensagem TEXT NOT NULL,
+    data_hora DATETIME DEFAULT CURRENT_TIMESTAMP
+);
