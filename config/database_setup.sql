bdaeda468de85c4ebc3f01331cd3ea66244469ef
dudufidelis <duduecal12@gmail.com>
2024-01-16 10:47:39 -0300

    alteração na tabela usuarios

--- config/database_setup.sql
+++ config/database_setup.sql
@@ -1,13 +1,17 @@
 -- Script SQL para criar tabelas
+-- Tabela usuarios
 CREATE TABLE usuarios (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome_usuario VARCHAR(255) NOT NULL,
-    senha_hash VARCHAR(255) NOT NULL
+    senha_hash VARCHAR(255) NOT NULL,
+    ip VARCHAR(45)
 );
 
+-- Tabela denuncias
 CREATE TABLE denuncias (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
     mensagem TEXT NOT NULL,
-    data_hora DATETIME DEFAULT CURRENT_TIMESTAMP
+    data_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
+    ip VARCHAR(45)
 );
