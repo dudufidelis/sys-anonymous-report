474c526cc014e85143ccae1cb324a62845609ff1
dudufidelis <duduecal12@gmail.com>
2024-01-17 10:47:15 -0300

    terminado css pagina administrativa

--- operators/administrativo.php
+++ operators/administrativo.php
@@ -40,76 +40,65 @@ $resultado = $conn->query($query);
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../css/admin-area.css">
     <link rel="shortcut icon" href="../assets/favicon.ico" type="image/x-icon">
     <title>Área Administrativa</title>
 </head>
+
 <body>
     <div class="container">
-        <h1>Área Administrativa</h1>
-        <a href="logout.php">Sair</a>
+        <header>
+            <img src="../assets/saine-logo.png" alt="Logo Saine Health Complex">
+            <h1>Área Administrativa</h1>
+            <a href="logout.php">Sair</a>
+        </header>
 
-        <form action="administrativo.php" method="GET">
-            <label for="data_filtro">Filtrar por Data:</label>
-            <input type="date" id="data_filtro" name="data_filtro" value="<?php echo $filtroData; ?>">
-            <button type="submit">Filtrar</button>
-        </form>
-
-        <h2>Denúncias:</h2>
-
-        <div class="navigation">
-            <?php
-            // Adicionar controles de navegação
-            $queryTotal = "SELECT COUNT(*) as total FROM denuncias WHERE 1 $condicaoData";
-            $resultadoTotal = $conn->query($queryTotal);
-            $totalDenuncias = $resultadoTotal->fetch_assoc()['total'];
-            $totalPaginas = ceil($totalDenuncias / $denunciasPorPagina);
-
-            // Definir o número máximo de links exibidos
-            $maxLinks = 5;
-
-            // Calcular o intervalo de páginas a serem exibidas
-            $intervalo = floor($maxLinks / 5);
-
-            // Calcular as páginas inicial e final a serem exibidas
-            $paginaInicial = max(1, $paginaAtual - $intervalo);
-            $paginaFinal = min($totalPaginas, $paginaInicial + $maxLinks - 1);
-
-            // Exibir botão "Anterior"
-            $paginaAnterior = max(1, $paginaAtual - 1);
-            echo "<a href='administrativo.php?pagina=$paginaAnterior&data_filtro=$filtroData'>&lt; Anterior</a> ";
-
-            // Exibir links para páginas dentro do intervalo calculado
-            for ($i = $paginaInicial; $i <= $paginaFinal; $i++) {
-                echo "<a href='administrativo.php?pagina=$i&data_filtro=$filtroData'>$i</a>";
-            }
-
-            // Exibir botão "Próxima"
-            $paginaProxima = min($totalPaginas, $paginaAtual + 1);
-            echo " <a href='administrativo.php?pagina=$paginaProxima&data_filtro=$filtroData'>Próxima &gt;</a>";
-
-            ?>
+        <div class="navigation-row">
+            <form action="administrativo.php" method="GET">
+                <label for="data_filtro">Data:</label>
+                <input type="date" id="data_filtro" name="data_filtro" value="<?php echo $filtroData; ?>">
+                <button type="submit">Filtrar</button>
+            </form>
+    
+            <div class="navigation">
+                <?php
+                $queryTotal = "SELECT COUNT(*) as total FROM denuncias WHERE 1 $condicaoData";
+                $resultadoTotal = $conn->query($queryTotal);
+                $totalDenuncias = $resultadoTotal->fetch_assoc()['total'];
+                $totalPaginas = ceil($totalDenuncias / $denunciasPorPagina);
+    
+                $paginaAnterior = max(1, $paginaAtual - 1);
+                echo "<a href='administrativo.php?pagina=$paginaAnterior&data_filtro=$filtroData'>Anterior</a> ";
+                echo "-";
+                $paginaProxima = min($totalPaginas, $paginaAtual + 1);
+                echo " <a href='administrativo.php?pagina=$paginaProxima&data_filtro=$filtroData'>Próxima</a>";
+                ?>
+            </div>
         </div>
 
-        <?php
-        // Exibir denúncias com IP
-        while ($row = $resultado->fetch_assoc()) {
-            // Formatando a data no formato dd/mm/yyyy
-            $dataFormatada = date('d/m/Y', strtotime($row['data_hora']));
-
-            echo "<p><strong>Data/Hora:</strong> $dataFormatada<br>";
-            echo "<strong>Nome:</strong> {$row['nome']}<br>";
-            echo "<strong>Denúncia:</strong> {$row['mensagem']}<br>";
-            echo "<strong>IP:</strong> {$row['ip']}</p>";
-            echo "<hr>";
-        }
-        ?>
-
+        <section>
+            <?php
+                while ($row = $resultado->fetch_assoc()) {
+                    $dataFormatada = date('d/m/Y', strtotime($row['data_hora']));
+                    echo "<div class='reports'>";
+                    echo "<div class='ip-row'>";
+                    echo "{$row['ip']}";
+                    echo "</div>";
+                    echo "<div class='report-info'>";
+                    echo "<p>Nome: {$row['nome']}</p>";
+                    echo "<p>Data: $dataFormatada</p>";
+                    echo "</div>";
+                    echo "<div class='bar'></div>";
+                    echo "<div class='report'>{$row['mensagem']}</div>";
+                    echo "</div>";
+                }
+            ?>
+        </section>
     </div>
-   
 </body>
 </html>
 
